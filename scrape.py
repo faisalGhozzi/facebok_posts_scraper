34962557175d3d0d764c515fa5076da6641dc5d1
unknown <faicel.ghozzi@esprit.tn>
2022-03-04 09:54:38 +0100

    init driver

---
+++ scrape.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+import time
+
+chrome_options = webdriver.ChromeOptions()
+prefs = {"profile.default_content_setting_values.notifications" : 2}
+chrome_options.add_experimental_option("prefs",prefs)
+
+driver = webdriver.Chrome('chromedriver.exe', chrome_options=chrome_options)
+driver.get("https://www.facebook.com")
+
