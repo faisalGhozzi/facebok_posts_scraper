bdd29ae65f4d36f324ad5dbca977f0680d2f1636
unknown <faicel.ghozzi@esprit.tn>
2022-03-04 10:20:50 +0100

    logged in to facebook

--- scrape.py
+++ scrape.py
@@ -5,6 +5,9 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 import time
 
+username = input("username : ")
+password = input("password : ")
+
 chrome_options = webdriver.ChromeOptions()
 prefs = {"profile.default_content_setting_values.notifications" : 2}
 chrome_options.add_experimental_option("prefs",prefs)
@@ -12,3 +15,12 @@ chrome_options.add_experimental_option("prefs",prefs)
 driver = webdriver.Chrome('chromedriver.exe', chrome_options=chrome_options)
 driver.get("https://www.facebook.com")
 
+username_field = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='email']")))
+password_field = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='pass']")))
+
+username_field.clear()
+username_field.send_keys(username)
+password_field.clear()
+password_field.send_keys(password)
+
+button = WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[type='submit']"))).click()
